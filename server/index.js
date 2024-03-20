3e78b0dcbec8e59f08033f8cdff106c113ecb81a
hoakhanhhl <lehoa@gmail.com>
2024-03-21 00:53:00 +0700

    update kalman

--- server/index.js
+++ server/index.js
@@ -1,23 +1,24 @@
 const express = require('express')
 const app = express()
 const port = 5000
+
+const KalmanFilter = require('kalmanjs');
+const kFilter = new KalmanFilter()
+let listAccX = []
+let fields = []
+let filteredData = null
 const cors = require('cors')
 
 app.use(cors())
-    //Thông tin kết nối mqtt
+    // Thông tin kết nối mqtt
 const mqtt = require('mqtt')
 const protocol = 'mqtt'
 const host = 'sinno.soict.ai'
 const mqttPort = '1883'
 const topic = 'dulieu'
 
-const { KalmanFilter } = require('kalman-filter')
-const kFilter = new KalmanFilter()
-let listAccx = []
-let fields = []
-let filteredData = null
 const connectUrl = `${protocol}://${host}:${mqttPort}`
-const client = mqtt.connect(connectUrl, {
+const clientMqtt = mqtt.connect(connectUrl, {
     clean: true,
     connectTimeout: 4000,
     username: 'student',
@@ -26,16 +27,16 @@ const client = mqtt.connect(connectUrl, {
 })
 
 // Thông báo kết quả kết nối mqtt server
-client.on('connect', () => {
+clientMqtt.on('connect', () => {
     console.log('Connected')
-    client.subscribe([topic], () => {
+    clientMqtt.subscribe([topic], () => {
         console.log(`Subscribe to topic 'dulieu'`)
     })
 })
 
 //Thông báo kết nối lỗi
-client.on('connect', () => {
-    client.publish(topic, 'nodejs mqtt test', { qos: 0, retain: false }, (error) => {
+clientMqtt.on('connect', () => {
+    clientMqtt.publish(topic, 'nodejs mqtt test', { qos: 0, retain: false }, (error) => {
         if (error) {
             console.error(error)
         }
@@ -43,25 +44,42 @@ client.on('connect', () => {
 })
 
 // In kết quả
-client.on('message', (topic, payload) => {
-    console.log('Received Message:', topic, payload.toString(), new Date())
-    const data = payload.toString().split(',')
-    if (data[2] && listAccx.length < 10) {
-        listAccx.push(Number([data[2]]))
-        console.log(listAccx)
-    }
-    if (listAccx.length === 10) {
-        filteredData = kFilter.filterAll(listAccx)
-        console.log(filteredData)
-    }
+clientMqtt.on('message', (topic, payload) => {
+    const data = payload.toString().split(',');
+    const listAccX = kFilter.filter(Number(data[2]));
+    const listAccY = kFilter.filter(Number(data[3]));
+    const listAccZ = kFilter.filter(Number(data[4]));
+
+    const listGyroX = kFilter.filter(Number(data[5]));
+    const listGyroY = kFilter.filter(Number(data[6]));
+    const listGyroZ = kFilter.filter(Number(data[7]));
+
+    const listAngX = kFilter.filter(Number(data[8]));
+    const listAngY = kFilter.filter(Number(data[9]));
+    const listAngZ = kFilter.filter(Number(data[10]));
+
+    console.log(listAccX);
+    console.log(listAccY);
+    console.log(listAccZ);
+
+    console.log(listGyroX);
+    console.log(listGyroY);
+    console.log(listGyroZ);
+
+    console.log(listAngX);
+    console.log(listAngY);
+    console.log(listAngZ);
+
+    console.log(data[0]);
+    console.log(data[1]);
 })
 
 app.get('/', (req, res) => {
     res.send("hello world")
 })
-app.get('/data', (req, res) => {
-    res.send({ filteredData: filteredData, listAccx: listAccx })
-})
+// app.get('/data', (req, res) => {
+//     res.send({ filteredData: filteredData, listAccx: listAccx })
+// })
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
